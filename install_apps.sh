3f5c8022bc607a0ea388e0ae8701c0706b6f8c46
Kamrul Islam <60686516+kamrul-islam3696@users.noreply.github.com>
2021-03-17 14:10:14 +0600

    Create install_apps.sh

---
+++ install_apps.sh
@@ -0,0 +1,36 @@
+sudo apt update
+sudo apt upgrade
+
+sudo apt install wget
+sudo apt install git
+
+sudo apt-get install -y \
+    terminator \
+    curl \
+    vim \
+    nano \
+    less
+
+#VLC Player
+#sudo apt install vlc
+
+#mpv player
+sudo apt install mpv
+
+#chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+#visual studio code
+sudo snap install --classic code
+
+#spotify
+sudo snap install spotify
+
+#skype
+sudo snap install skype --classic
+
+
+
+
+
